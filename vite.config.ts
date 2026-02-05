98dc42d1ebe3595b7d87501262aa81f87c469e81
Jackie Li <aurorali99@gmail.com>
2026-02-05 15:53:25 -0500

    Fix configuration error.

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,8 @@
-import rawPlugin from 'vite-raw-plugin';
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
 export default defineConfig({
     build: {
         target: 'esnext'
     },
-    base: process.env.GITHUB_ACTIONS_BASE || undefined,
-    plugins: [
-        rawPlugin({
-            fileRegex: /\.wgsl$/,
-        }),
-        react()
-    ],
+    base: process.env.GITHUB_ACTIONS_BASE || undefined
 })
